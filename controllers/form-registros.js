5a7fc3009e78793ffcc3c06d3c26eea18ab26d3a
David Gutierrez <gutierrezdvde@gmail.com>
2022-07-09 22:03:48 -0300

    Form-registros dinamico

--- controllers/form-registros.js
+++ controllers/form-registros.js
@@ -7,23 +7,28 @@ module.exports.index = (req, res) => {
 }
 
 module.exports.store = (req, res) => {
-    
-    connection.query('INSERT INTO registros SET ?', 
-        { usuario: req.session.user,
-          tipo: req.body.inputGroupSelect01, 
-          ot: req.body.ot,
-          nombre_cliente: req.body.nombre,
-          identificacion: req.body.identificacion,
-          contrato: req.body.contrato,
-          direccion: req.body.direccion,
-          telefono: req.body.telefono,
-          equipoi: req.body.equipoi,
-          equipor: req.body.equipor
-        }, 
+    connection.query(
+        "INSERT INTO registros SET ?",
+        {
+            usuario: req.session.user.toUpperCase(),
+            tipo: req.body.inputGroupSelect01.toUpperCase(),
+            ot: req.body.ot,
+            nombre_cliente: req.body.nombre.toUpperCase(),
+            apellido_cliente: req.body.apellido.toUpperCase(),
+            identificacion: req.body.identificacion,
+            contrato: req.body.contrato,
+            direccion: req.body.direccion.toUpperCase(),
+            telefono: req.body.telefono,
+            equipoi: req.body.equipoi.toUpperCase(),
+            equipor: req.body.equipor.toUpperCase(),
+            comentario: req.body.comentario.toUpperCase()
+        },
         (error, results) => {
-            
-        if (error) { throw error }
-        console.log(results);
-        res.redirect('/form-registros');
-    });
-} 
+            if (error) {
+                throw error;
+            }
+            console.log(results);
+            res.redirect("/form-registros");
+        }
+    );
+}; 
