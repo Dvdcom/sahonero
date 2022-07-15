e983a7a9b98706660abf6667dfc17c07cd61e770
David Gutierrez <gutierrezdvde@gmail.com>
2022-07-15 12:21:25 -0300

    un commit

--- controllers/mis-registros.js
+++ controllers/mis-registros.js
@@ -26,7 +26,41 @@ module.exports.index = (req, res) => {
             if (endingLink < (page + 4)) {
                 iterator -= (page + 4) - numberOfPages;
             }
-            res.render('mis-registros/index',{registros: result, page, iterator, endingLink, numberOfPages,layout: './layouts/layout-navbar'});
+            res.render('mis-registros/index',{registros: result, page, iterator,numOfResults, endingLink, numberOfPages,layout: './layouts/layout-navbar'});
+
+        });
+    });
+};
+
+module.exports.filtrar = (req, res) => {
+    let sql = 'SELECT * FROM registros WHERE tipo="instalacion" ';
+    connection.query(sql, (error, result) => {
+        if (error) { throw error }
+        const numOfResults = result.length;
+        const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+        let page = req.query.page ? Number(req.query.page) : 1;
+        if (page > numberOfPages) {
+            res.redirect('/?page=' + encodeURIComponent(numberOfPages));
+        } else if (page < 1) {
+            res.redirect('/?page=' + encodeURIComponent('1'));
+        }
+        const startingLimit = (page - 1) * resultsPerPage;
+        sql = `SELECT * FROM registros WHERE tipo="instalacion" ORDER BY fecha DESC LIMIT ${startingLimit},${resultsPerPage}`;
+
+        connection.query(sql, (error, result) => {
+            if (error) { throw error }
+            //si cambio resultsPerPage tengo que cambiar iterator
+            console.log(page - 5);
+            console.log(page);
+            let iterator = (page - 5) < 1 ? 1 : page - 1;
+            console.log(iterator);
+
+            let endingLink = (iterator + 9) <= numberOfPages ? (iterator + 9) : page + (numberOfPages - page);
+            
+            if (endingLink < (page + 4)) {
+                iterator -= (page + 4) - numberOfPages;
+            }
+            res.render('mis-registros/filtrar',{registros: result, page, iterator,numOfResults, endingLink, numberOfPages,layout: './layouts/layout-navbar'});
 
         });
     });
--- routes/mis-registros.js
+++ routes/mis-registros.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require('../controllers/mis-registros');
 
 router.get('/mis-registros', controller.index);
+router.get('/mis-registros/filtrar', controller.filtrar);
 
 module.exports = router;
