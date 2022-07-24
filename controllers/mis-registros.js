4f944838cac4cb66272d07fa3e9913a0c5e82ace
David Gutierrez <gutierrezdvde@gmail.com>
2022-07-24 17:40:19 -0300

    Se implementa datatablet

--- controllers/mis-registros.js
+++ controllers/mis-registros.js
@@ -5,28 +5,8 @@ const resultsPerPage = 20;
 module.exports.index = (req, res) => {
     let sql = 'SELECT * FROM registros';
     connection.query(sql, (error, result) => {
-        if (error) { throw error }
-        const numOfResults = result.length;
-        const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
-        let page = req.query.page ? Number(req.query.page) : 1;
-        if (page > numberOfPages) {
-            res.redirect('/?page=' + encodeURIComponent(numberOfPages));
-        } else if (page < 1) {
-            res.redirect('/?page=' + encodeURIComponent('1'));
-        }
-        const startingLimit = (page - 1) * resultsPerPage;
-        sql = `SELECT * FROM registros ORDER BY fecha DESC LIMIT ${startingLimit},${resultsPerPage}`;
-        connection.query(sql, (error, result) => {
-            if (error) { throw error }
-            //si cambio resultsPerPage tengo que cambiar iterator
-            let iterator = (page - 5) < 1 ? 1 : page - 5;
-            let endingLink = (iterator + 9) <= numberOfPages ? (iterator + 9) : page + (numberOfPages - page);
-            if (endingLink < (page + 4)) {
-                iterator -= (page + 4) - numberOfPages;
-            }
-            res.render('mis-registros/index',{registros: result, page, iterator,numOfResults,resultsPerPage, endingLink, numberOfPages,layout: './layouts/layout-navbar'});
+            res.render('mis-registros/index',{registros: result,layout: './layouts/layout-datatablet'});
         });
-    });
 };
 
 
--- public/js/mregistros.js
+++ public/js/mregistros.js
@@ -1,2 +1,35 @@
-/* const btn-ocultar = document.querySelector('.buton-ocultar');
-const columnocultar = document.querySelectorAll('.dato-oc'); */
+
+$(document).ready(function () {
+    $('#miTablet').DataTable({
+        "lengthMenu": [ 15, 20, 50, 75, 100 ],
+        "order": [0,'desc'],
+        language: {
+            processing: "Procesando...",
+            search: "Buscar :",
+            lengthMenu: "Mostrar _MENU_ entradas",
+            info: "Mostrando _START_ a _END_ de _TOTAL_ entradas.",
+            infoEmpty: "Mostrando 0 a 0 de 0 entradas",
+            infoFiltered: "(Filtrado de _MAX_ entradas totales)",
+            infoPostFix: "",
+            loadingRecords: "Cargando...",
+            zeroRecords: "No se encontraron registros que coincidan con la búsqueda",
+                paginate: {
+                        first: "Primera",
+                        last: "Ultima",
+                        next: "Siguiente",
+                        previous: "Anterior"
+                    },
+            aria: {
+                sortAscending: ": activer pour trier la colonne par ordre croissant",
+                sortDescending: ": activer pour trier la colonne par ordre décroissant"
+            }
+        }
+    });
+    
+});
+
+$(function() {
+    $('#datepicker').datepicker();
+    $('#datepicker2').datepicker();
+});
+
